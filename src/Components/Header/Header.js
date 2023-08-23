030db79b41444b34ba78611191f60dfb30e1dd29
Vinayak Agarwal <vinayakaggarwal05@gmail.com>
2023-08-23 23:57:41 +0530

    fixed search ui

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -13,18 +13,16 @@ const Header = ({ page, setPage, setShowDialog, setSearch }) => {
       <nav className={`nav flex align-items-center`}>
         {
           page &&
-          <div className="pointer" style={{ marginRight: "20px" }} onClick={() => setPage(() => "")}>
+          <div className="pointer" onClick={() => setPage(() => "")}>
             <img className="back-button" src="/img/previous.png"></img>
           </div>
         }
         <h1 className={"nav-title"}>{!page ? <>Gallery</> : page}</h1>
-        <div className={`flex navbar-buttons`}>
-          <div className={`search-bar flex align-items-center`}>
-            <SearchNormal1 size="30" color="var(--white-100)" />
-            <input type="text" className={"search-input"} placeholder={placeholder} onChange={(e) => setSearch(e.target.value)} />
-          </div>
-          <Button onClick={() => setShowDialog(() => true)}>{!page ? <> + Add Folder</> : <> + Add Image</>}</Button>
+        <div className={`search-bar flex align-items-center`}>
+          <SearchNormal1 size="30" color="var(--white-100)" />
+          <input type="text" className={"search-input"} placeholder={placeholder} onChange={(e) => setSearch(e.target.value)} />
         </div>
+        <Button onClick={() => setShowDialog(() => true)}>{!page ? <> + Add Folder</> : <> + Add Image</>}</Button>
       </nav>
     </>
   )
